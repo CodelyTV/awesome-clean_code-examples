e5684ea03fa2e5a8b3208267977192eb82a88fe4
Rafa Gómez <rgomezcasas@gmail.com>
2024-08-19 16:20:59 +0200

    feat(comments): add example

--- code-comments/python/2-with_good_comments/src/user_controller.py
+++ code-comments/python/2-with_good_comments/src/user_controller.py
@@ -8,9 +8,6 @@ class UserController:
 		self.user_repository = user_repository
 
 	def create_user(self, username: str, email: str) -> Dict[str, Any]:
-		"""
-		Create a new user.
-		"""
 		user = {
 			'username': username,
 			'email': email
@@ -19,7 +16,4 @@ class UserController:
 		return self.user_repository.save(user)
 
 	def get_user(self, user_id: int) -> Optional[Dict[str, Any]]:
-		"""
-		Get a user by ID.
-		"""
 		return self.user_repository.find_by_id(user_id)
--- code-comments/python/2-with_good_comments/src/user_repository.py
+++ code-comments/python/2-with_good_comments/src/user_repository.py
@@ -9,15 +9,9 @@ class UserRepository:
 		self.db_params = db_params
 
 	def save(self, user: Dict[str, any]) -> Dict[str, any]:
-		"""
-		Save a user in database.
-		"""
 		with psycopg2.connect(**self.db_params) as conn:
 			with conn.cursor() as cur:
-				# If user has id, it means it already exists in database, so we have to update it
-				# Otherwise, we have to insert a new user
-				if 'id' not in user:
-					# Update existing user
+				if self.is_new_user(user):
 					cur.execute('''
 						INSERT INTO users (name, email, age)
 						VALUES (%s, %s, %s)
@@ -25,13 +19,23 @@ class UserRepository:
 					''', (user.get('name'), user.get('email'), user.get('age')))
 					user['id'] = cur.fetchone()[0]
 				else:
-					# Insert new user
 					cur.execute('''
 						UPDATE users
 						SET name = %s, email = %s, age = %s
 						WHERE id = %s
 					''', (user.get('name'), user.get('email'), user.get('age'), user['id']))
 
+					# We also update the user in the ERP system.
+					#
+					# Context:
+					# 1. When we create a user in our system, we also create it in the ERP system via their API.
+					# 2. However, the ERP system does not provide an endpoint for updating users.
+					#
+					# Solution:
+					# To keep both systems in sync, we directly connect to the ERP's database
+					# and update the user information there.
+					#
+					# Note: This is a temporary solution until the ERP provides an official update method.
 					cur.execute('''
 						UPDATE erp_users
 						SET name = %s, email = %s, age = %s
@@ -42,11 +46,10 @@ class UserRepository:
 
 		return user
 
+	def is_new_user(self, user):
+		return 'id' not in user
 
 def find_by_id(self, user_id: int) -> Optional[Dict[str, any]]:
-	"""
-	Search a user by id.
-	"""
 	with psycopg2.connect(**self.db_params) as conn:
 		with conn.cursor(cursor_factory=DictCursor) as cur:
 			cur.execute('SELECT * FROM users WHERE id = %s', (user_id,))
