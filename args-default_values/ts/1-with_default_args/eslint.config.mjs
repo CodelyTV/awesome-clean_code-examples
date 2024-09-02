53b48eb2155c1fb53dc672e3883daee808ee3acc
Rafa Gómez <rgomezcasas@gmail.com>
2024-09-02 18:33:24 +0200

    feat(default-args): tune base

---
+++ args-default_values/ts/1-with_default_args/eslint.config.mjs
@@ -0,0 +1,28 @@
+import eslintConfigNext from "@next/eslint-plugin-next";
+import eslintConfigCodely from "eslint-config-codely";
+import globals from "globals";
+
+export default [
+	...eslintConfigCodely.course,
+	{
+		...eslintConfigNext.configs.recommended,
+		plugins: {
+			"@next/next": eslintConfigNext,
+		},
+		settings: {
+			react: {
+				version: "detect",
+			},
+		},
+		languageOptions: {
+			parserOptions: {
+				ecmaFeatures: {
+					jsx: true,
+				},
+			},
+			globals: {
+				...globals.browser,
+			},
+		},
+	},
+];
