5af960f81eabcd0fbfb95248c97ffff653a7e172
Rafa Gómez <rgomezcasas@gmail.com>
2024-09-04 15:35:43 +0200

    feat(default-args): add bad example

--- args-default_values/ts/1-with_default_args/tests/home.spec.ts
+++ args-default_values/ts/1-with_default_args/tests/home.spec.ts
@@ -2,38 +2,52 @@ import { expect, Page, test } from "@playwright/test";
 
 test.describe("Home page should", () => {
 	test("add a product to the cart", async ({ page }) => {
-		await goToHomePage(page);
+		await givenTheHomePageIsLoaded(page);
 
-		await clickAddToCart(page, 1);
+		await whenClickToAddProductToCart(page, 1);
 
-		await expectCartToContainTotalElements(page, 1);
-		await expectCartToContainProduct(page, "El mejor teclado mecánico");
-		await expectTotalPriceToBe(page, "$129.99");
+		await thenTheCartShouldContainTotalElements(page, 1);
+		await thenTheCartShouldContain(page, "El mejor teclado mecánico");
+		await thenTheTotalPriceShouldBe(page, "$129.99");
+	});
+
+	test("add a free product to the cart", async ({ page }) => {
+		await givenTheHomePageIsLoaded(page);
+
+		await whenClickToFreeProductLink(page);
+
+		await thenTheCartShouldContainTotalElements(page, 1);
+		await thenTheCartShouldContain(page, "Código de Javi firmado con gpg");
+		await thenTheTotalPriceShouldBe(page, "$0.00");
 	});
 
 	test("calculate the cart total", async ({ page }) => {
-		await goToHomePage(page);
+		await givenTheHomePageIsLoaded(page);
 
-		await clickAddToCart(page, 1);
-		await clickAddToCart(page, 2);
+		await whenClickToAddProductToCart(page, 1);
+		await whenClickToAddProductToCart(page, 2);
 
-		await expectCartToContainTotalElements(page, 2);
-		await expectTotalPriceToBe(page, "$189.98");
+		await thenTheCartShouldContainTotalElements(page, 2);
+		await thenTheTotalPriceShouldBe(page, "$189.98");
 	});
 });
 
-async function goToHomePage(page: Page): Promise<void> {
+async function givenTheHomePageIsLoaded(page: Page): Promise<void> {
 	await page.goto("http://127.0.0.1:3000");
 }
 
-async function clickAddToCart(
+async function whenClickToAddProductToCart(
 	page: Page,
 	productNumber: number,
 ): Promise<void> {
 	await page.click(`text=Añadir al carrito >> nth=${productNumber - 1}`);
 }
 
-async function expectCartToContainTotalElements(
+async function whenClickToFreeProductLink(page: Page): Promise<void> {
+	await page.click(`text=Regalo para ti`);
+}
+
+async function thenTheCartShouldContainTotalElements(
 	page: Page,
 	expectedTotalElements: number,
 ): Promise<void> {
@@ -44,7 +58,7 @@ async function expectCartToContainTotalElements(
 	).toBe(expectedTotalElements);
 }
 
-async function expectCartToContainProduct(
+async function thenTheCartShouldContain(
 	page: Page,
 	expectedProductName: string,
 ) {
@@ -56,7 +70,10 @@ async function expectCartToContainProduct(
 	).toBe(expectedProductName);
 }
 
-async function expectTotalPriceToBe(page: Page, expectedTotalPrice: string) {
+async function thenTheTotalPriceShouldBe(
+	page: Page,
+	expectedTotalPrice: string,
+) {
 	expect(
 		await page
 			.locator("text=Total:")
