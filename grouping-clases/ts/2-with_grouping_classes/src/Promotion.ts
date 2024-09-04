663cb61135fad752aa55828fd98928f3686d7ec0
Rafa Gómez <rgomezcasas@gmail.com>
2024-09-04 16:36:34 +0200

    feat(grouping_clases): add good example

--- grouping-clases/ts/2-with_grouping_classes/src/Promotion.ts
+++ grouping-clases/ts/2-with_grouping_classes/src/Promotion.ts
@@ -3,6 +3,6 @@ import { DateRange } from "./DateRange";
 export class Promotion {
 	constructor(
 		readonly dateRange: DateRange,
-		discountPercentage: number,
+		readonly discountPercentage: number,
 	) {}
 }
